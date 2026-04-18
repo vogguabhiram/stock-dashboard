83a410c0c817fea9a26762bfe033407344776417
Voggu abhiram <vogguabhiram2005@gmail.com>
2026-04-18 21:28:12 +0530

    removed vite proxy

--- Real Stock/client/vite.config.js
+++ Real Stock/client/vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://api.voarion.in',
-        changeOrigin: true
-      }
-    }
-  }
+  plugins: [react()]
 })
