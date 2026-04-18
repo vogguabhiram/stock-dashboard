9791b6eaa246de37ef5c7d12a7551515a8e42007
Voggu abhiram <vogguabhiram2005@gmail.com>
2026-04-18 21:00:45 +0530

    removed localhost completely

--- Real Stock/server/index.js
+++ Real Stock/server/index.js
@@ -27,12 +27,9 @@ app.use(express.json())
 app.use(limiter)
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/stockdash', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err))
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err))
 
 // Routes
 app.use('/api/stocks', stockRoutes)
